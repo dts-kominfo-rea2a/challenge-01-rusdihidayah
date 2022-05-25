73238b25128de3cbe10f9f28120e3035e70fd884
Rusdi Hidayah <rusdi.hidayah11@gmail.com>
2022-05-25 23:10:00 +0700

    Bimillah tugas JS Struktur Data

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ education: Array
 favoriteRestaurant: Array
 */
 
+const firstUserFavResto = ['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi'];
+const secondUserFavResto = ['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy'];
+
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 const firstUser = {
     name : 'Monica',
@@ -20,7 +23,7 @@ const firstUser = {
     favoriteColor : new Set('Yellow', 'Pink', 'White', 'Purple'),
     isHavePet : 'Yes',
     education : [['name : SD 01', 'city : Jakarta', 'graduate : 2016'], ['name : SMP 02', 'city: Jakarta', 'graduate : 2019'], ['name : SMA 03', 'city : Tanggerang']],
-    favoriteRestaurant : ['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']
+    favoriteRestaurant : [...new Set(firstUserFavResto)]
 };
 const secondUser = {
     name : 'Wendy',
@@ -30,7 +33,7 @@ const secondUser = {
     favoriteColor : new Set('Blue', 'Black', 'Grey'),
     isHavePet : 'No',
     education : [['name : SD 02', 'city : Jakarta', 'graduate : 2010'],['name : SMP 03', 'city : Bogor', 'graduate : 2013'],['name : SMA 01', 'city : Surabaya', 'graduate : 2016'],['name : University Maju', 'city: Tanggerang']],
-    favoriteRestaurant : ['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy']
+    favoriteRestaurant : [...new Set(secondUserFavResto)]
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
